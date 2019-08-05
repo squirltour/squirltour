e07f3160372aebbc2bb479fc8803db5c203cb8c2
nicvhub <nicolasvenegasparker@gmail.com>
2019-08-05 16:26:07 -0700

    style v1

--- client/components/NavOptions.jsx
+++ client/components/NavOptions.jsx
@@ -24,8 +24,9 @@ class NavOptions extends Component{
     render(){
         return(
             <Fragment>
-            {Object.keys(this.props.itinerary).length !== undefined ? 
-            <div>{Object.keys(this.props.itinerary).length}</div> : null}
+            {Object.keys(this.props.itinerary).length !== undefined &&  Object.keys(this.props.itinerary).length > 0? 
+            <div className="itinerary-items">Itinerary Items {Object.keys(this.props.itinerary).length}</div> : null}
+            
             {/* <Register startRegistration={this.props.startRegistration} completeRegistration={this.props.completeRegistration}/> */}
             {/* <Login/>
             <Itinerary/>
